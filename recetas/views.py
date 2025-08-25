c95e93b5fc94dc6eb74930a0c9168e6ea4c94ab9
OmarVelasquezR <164939773+OmarVelasquezR@users.noreply.github.com>
2025-08-25 04:03:11 -0500

    Smart Food 24 - Editar y Eliminar receta mejorado

--- recetas/views.py
+++ recetas/views.py
@@ -81,28 +81,50 @@ class CrearRecetaView(LoginRequiredMixin, CreateView):
 # Vista para editar receta
 @login_required
 def editar_receta(request, pk):
-    receta = get_object_or_404(Receta, pk=pk)
-    if request.method == 'POST':
-        form = RecetaForm(request.POST, request.FILES, instance=receta)
-        if form.is_valid():
-            receta = form.save(commit=False)
-            nombres = request.POST.getlist('ingredientes[nombre][]')
-            cantidades = request.POST.getlist('ingredientes[cantidad][]')
-            unidades = request.POST.getlist('ingredientes[unidad][]')
-            receta.ingredientes = [
-                {"nombre": n, "cantidad": c, "unidad": u}
-                for n, c, u in zip(nombres, cantidades, unidades)
-                if n or c
-            ]
-            receta.instrucciones = request.POST.getlist('instrucciones[]')
-            receta.categorias = request.POST.get('categorias', '')
-            receta.alergias = request.POST.get('alergias', '')
+    receta = get_object_or_404(Receta, pk=pk, autor=request.user)
+    categorias_default = ["Desayunos", "Almuerzos", "Cenas", "Postres", "Bebidas", "Vegetariano", "Vegano"]
+    alergias_default = ["Leche", "Huevo", "Maní", "Frutos secos", "Soja", "Gluten", "Pescado", "Marisco"]
 
-            receta.save()
-            return redirect('mis_recetas')
-    else:
-        form = RecetaForm(instance=receta)
-    return render(request, 'recetas/editar_receta.html', {'form': form, 'receta': receta})
+    if request.method == 'POST':
+        # Campos simples
+        receta.titulo = request.POST.get('titulo')
+        receta.descripcion = request.POST.get('descripcion')
+        receta.dificultad = request.POST.get('dificultad')
+        receta.tiempo = request.POST.get('tiempo')
+        receta.porciones = request.POST.get('porciones')
+        receta.categorias = request.POST.get('categorias', '')
+        receta.alergias = request.POST.get('alergias', '')
+
+        # Imagen principal
+        if 'imagen' in request.FILES:
+            receta.imagen = request.FILES['imagen']
+
+        # Más fotos (si manejas campo en el modelo)
+        if 'fotos_extra' in request.FILES:
+            receta.fotos_extra = request.FILES.getlist('fotos_extra')
+
+        # Ingredientes dinámicos
+        nombres = request.POST.getlist('ingredientes[nombre][]')
+        cantidades = request.POST.getlist('ingredientes[cantidad][]')
+        unidades = request.POST.getlist('ingredientes[unidad][]')
+        receta.ingredientes = [
+            {"nombre": n, "cantidad": c, "unidad": u}
+            for n, c, u in zip(nombres, cantidades, unidades)
+            if n or c
+        ]
+
+        # Instrucciones dinámicas
+        receta.instrucciones = request.POST.getlist('instrucciones[]')
+
+        # Guardar cambios
+        receta.save()
+        return redirect('mis_recetas')
+
+    return render(request, 'recetas/editar_receta.html', {
+        'receta': receta,
+        'categorias_default': categorias_default,
+        'alergias_default': alergias_default,
+    })
 
 # Vista para eliminar receta
 @login_required
